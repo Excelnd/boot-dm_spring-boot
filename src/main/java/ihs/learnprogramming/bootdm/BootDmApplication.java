22a26eb302d4fbf30ddc6e0a3ff25cd09be86e07
Excelnd <ihsanmpm@gmail.com>
2019-11-19 21:34:57 +0530

    application properties

---
+++ src/main/java/ihs/learnprogramming/bootdm/BootDmApplication.java
@@ -0,0 +1,13 @@
+package ihs.learnprogramming.bootdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootDmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootDmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ihs/learnprogramming/bootdm/BootDmApplicationTests.java
@@ -0,0 +1,16 @@
+package ihs.learnprogramming.bootdm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootDmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
